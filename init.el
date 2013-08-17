cc21c1a8ad99e0f20b00c82af632e5657ba8dc63
Ulf Ejlertsson <ulf.ejlertsson@gmail.com>
2013-08-17 15:35:33 +0200

    Minor changes, included coq support etc.

--- init.el
+++ init.el
@@ -36,6 +36,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(agda2-include-dirs (quote ("/Users/ulf/Dev/haskell/lib-0.6/src")))
  '(custom-enabled-themes (quote (zenburn)))
  '(custom-safe-themes (quote ("31e407508d9a61e1f7cd7933d52612f3e440b89f8da48fe56e4e483c227ae7b9" "f77fa50af9de18450bcfd2f9c27bc69fa1693417" default)))
  '(quack-global-menu-p nil)
