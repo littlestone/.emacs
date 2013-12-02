48758a6d93eab1e08c32691eb825eee389c1621e
Ulf Ejlertsson <ulf.ejlertsson@gmail.com>
2013-12-02 20:18:36 +0100

    New and improved emacs configuration

--- init.el
+++ init.el
@@ -1,12 +1,11 @@
-;;
-;; -*- Emacs-Lisp -*-
-;; ---------------------------------------------------------------------------
-;; init.el
-;; ---------------------------------------------------------------------------
-;;;;
-;; This file loads Org-mode and then loads the rest of our Emacs initialization
-;; from Emacs lisp embedded in literate Org-mode files.
-;; Load up Org Mode and (now included) Org Babel for elisp embedded in Org Mode files
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
+
+(require 'pallet)
+
+;; This file loads org-mode and then loads the rest of our Emacs initialization
+;; from Emacs lisp embedded in literate org-mode files.
+;; Load up org-mode and (now included) org babel for elisp embedded in org mode files
 (setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
 
 (let* ((org-dir (expand-file-name
@@ -19,35 +18,13 @@
                                             ".." org-dir))))
        (load-path (append (list org-dir org-contrib-dir)
                           (or load-path nil))))
-  ;; load up Org-mode and Org-babel
-  (require 'org-install)
-  (require 'ob-tangle))
+
+;; load up org-mode and org-babel
+(require 'org-install)
+(require 'ob-tangle))
 
 ;; load up all literate org-mode files in this directory
 (mapc #'org-babel-load-file (directory-files dotfiles-dir t "\\.org$"))
 
-
-
 (org-babel-load-file (expand-file-name "emacs-config.org" user-emacs-directory))
 
-;; We do not want to mess with these...
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(agda2-include-dirs (quote ("/Users/ulf/Dev/haskell/lib-0.6/src")))
- '(custom-enabled-themes (quote (zenburn)))
- '(custom-safe-themes (quote ("31e407508d9a61e1f7cd7933d52612f3e440b89f8da48fe56e4e483c227ae7b9" "f77fa50af9de18450bcfd2f9c27bc69fa1693417" default)))
- '(quack-global-menu-p nil)
- '(send-mail-function (quote smtpmail-send-it))
- '(smtpmail-smtp-server "smtp.gmail.com")
- '(smtpmail-smtp-service 587))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-;; End of File
