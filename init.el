5157873a846a363779fd31423ebb3d1adc5ff537
Ulf Ejlertsson <ulf.ejlertsson@gmail.com>
2014-08-14 22:33:21 +0200

    Minor tweaks to Ocaml and Haskell mode. Also minor changes to structure.

--- init.el
+++ init.el
@@ -28,3 +28,16 @@
 
 (org-babel-load-file (expand-file-name "emacs-config.org" user-emacs-directory))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("146d24de1bb61ddfa64062c29b5ff57065552a7c4019bee5d869e938782dfc2a" "a53714de04cd4fdb92ed711ae479f6a1d7d5f093880bfd161467c3f589725453" "11d069fbfb0510e2b32a5787e26b762898c7e480364cbc0779fe841662e4cf5d" default)))
+ '(powerline-default-separator (quote arrow)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
